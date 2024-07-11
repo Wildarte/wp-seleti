6b4fbc6f24ede9e01c8f78abfc61548e1979d1c5
wildarte <wildarte10@gmail.com>
2024-07-10 22:41:06 -0300

    continucao

---
+++ footer.php
@@ -0,0 +1,47 @@
+    <footer class="footer">
+        <div class="container d-flex content-min">
+            <div class="col_footer d-flex">
+                <a href="" class="logo_footer">
+                    <img src="assets/img/logo_2023.png" alt="">
+                </a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, velit! Ullam modi velit rem consectetur ab, commodi</p>
+            </div>
+            <div class="col_footer">
+                <h3>Menu</h3>
+
+                <ul>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Todas as Vagas</a></li>
+                    <li><a href="">Políticas de Privacidade</a></li>
+                    <li><a href="">Termos de Uso</a></li>
+                </ul>
+            </div>
+            <div class="col_footer">
+                <h3>Categorias</h3>
+
+                <ul>
+                    <li><a href="">Administração</a></li>
+                    <li><a href="">Contabilidade</a></li>
+                    <li><a href="">Serviços Gerais</a></li>
+                    <li><a href="">Comercial</a></li>
+                    <li><a href="">Marketing</a></li>
+                    <li><a href="">Metalurgica</a></li>
+                </ul>
+            </div>
+            <div class="col_footer">
+                <h3>Redes Sociais</h3>
+            </div>
+        </div>
+        <div class="bottom_footer">
+            <div class="container d-flex justify-center p-10">
+                <p>@ Todos od direitos reservados</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/js/script.js"></script>
+    <?php wp_footer() ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Seleti Empregos</title>
+    <?php wp_head() ?>
+</head>
+<body>
+    
+    <header class="header">
+        <div class="container content_header">
+            <a href="" class="col_header">
+                <img src="assets/img/logo_2023.png" alt="">
+            </a>
+
+            <nav class="menu col_header d-flex justify-center">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+
+            <div class="right_header col_header d-flex justify-end">
+                <a href="" class="btn-default">Ver Vagas</a>
+            </div>
+        </div>
+    </header>
